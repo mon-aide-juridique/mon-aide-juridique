c75725022d0c41100f2b3aefa401fc0713c564dc
Adrien VERGNAUD <a.vergnaud@cat-amania.com>
2024-07-17 19:39:45 +0200

    sso google

---
+++ app/components/Hello.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Hello() {
+
+    const { data: session } = useSession()
+
+    // checking if sessions exists
+    if (session) {
+        //logged in code
+        return (
+            <div>
+                <p>Welcome {session.user?.name}. Signed In As</p>
+                <p>{session.user?.email}</p>
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <p>Not Signed In</p>
+            <button onClick={() => signIn('google')}>Sign in with google</button>
+        </div>
+    );
+};
